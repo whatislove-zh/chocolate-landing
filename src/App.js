91b6ee9b413cc590aa98269c00906ee8d6086d31
Vladislav <vladgm233223@gmail.com>
2023-06-15 17:09:54 +0300

    created the project structure

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Header from "./components/header/Header";
+import Banner from "./components/banner/Banner.jsx";
+import Benefits from "./components/benefits/Benefits.jsx";
+import Ingredients from "./components/ingredients/Ingredients.jsx";
+import HowMade from "./components/how-made/How-made.jsx";
+import Products from "./components/products/Products.jsx";
+import TopSellers from "./components/top-sellers/Top-sellers.jsx";
+import Reviews from "./components/reviews/Reviews.jsx";
+import Subscribe from "./components/subscribe/Subscribe.jsx";
+import Footer from "./components/footer/Footer.jsx";
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <>
+      <Header />
+      <Banner />
+      <Benefits />
+      <Ingredients />
+      <HowMade />
+      <Products />
+      <TopSellers />
+      <Reviews />
+      <Subscribe />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/top-sellers/Top-sellers.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TopSellers() {
+  return (
+    <div>TopSellers</div>
+  )
+}
