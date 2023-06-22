e7238cd0ff20820d657ffc7aeb79989c5a2580c6
Vladislav <vladgm233223@gmail.com>
2023-06-22 19:13:36 +0300

    add initial ctructure of product section

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
 
 export default function Products() {
   return (
-    <div>Products</div>
-  )
+    <section className="products-section">
+      <div className="container">
+        <div className="products-wrapper">
+          <h2 className="products-header common-header">
+            OUR <span className="orange-text">PRODUCTS</span>
+          </h2>
+          <div className="products-slider">
+            
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
