e5ad378b9f3195efb84eb7c701ae73870f4ad3c5
Vladislav <vladgm233223@gmail.com>
2023-06-24 00:22:19 +0300

    add review section

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from "react";
+import ReviewsSlider from "./ReviewsSlider";
 
 export default function Reviews() {
   return (
-    <div>Reviews</div>
-  )
+    <section className="reviews-section">
+      <div className="container">
+        <div className="reviews-wrapper">
+          <h2 className="reviews-header common-header">
+            <span className="orange-text">CHOCOLATE </span>IS LOVED
+          </h2>
+          <ReviewsSlider />
+          <button className="common-button review-button">Leave a review</button>
+        </div>
+      </div>
+    </section>
+  );
 }
